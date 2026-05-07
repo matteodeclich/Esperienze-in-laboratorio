93fd454cbbf80a26c0df21a8bda398b3b2f02a98
s3kir-o <fedebernabei8@gmail.com>
2026-05-07 17:41:46 +0200

    Update analisi_dati.py

--- Libreria python/analisi_dati.py
+++ Libreria python/analisi_dati.py
@@ -1,5 +1,20 @@
 import numpy as np
+import matplotlib as mtp
+import scipy as sp
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import sys
+from scipy.stats import norm
+from scipy.stats import chi2
+from matplotlib.ticker import FormatStrFormatter
 from scipy import stats
+from scipy.constants import g
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.ticker import StrMethodFormatter
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
+from uncertainties.umath import sqrt
 from uncertainties import ufloat
  
  
@@ -138,3 +153,4 @@ def compatibilita_valore(misura, valore_vero, nome="misura"):
     print(f"Compatibilità di {nome} con {valore_vero:.8g}: |t| = {t:.8g}")
  
     return t
+
