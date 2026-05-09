f5e0f11f0c9867701e95c8e0e3a0891c834e971b
mdeclich-glitch <m.declich@campus.unimib.it>
2026-05-09 17:53:02 +0200

    Aggiornamento file excel

---
+++ Libreria_python/librerie_utili.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib as mtp
+import scipy as sp
+import matplotlib.pyplot as plt
+import pandas as pd
+import math
+import sys
+from scipy.stats import norm
+from scipy.stats import chi2
+from matplotlib.ticker import FormatStrFormatter
+from scipy import stats
+from scipy.constants import g
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.ticker import StrMethodFormatter
+
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
+from uncertainties.umath import sqrt
+from uncertainties import ufloat
+ 
